b74c321162c973138b71d8630ea6bf665c825915
M.Jackson <mwj8410@gmail.com>
2016-07-15 21:26:23 +0000

    considering removing components

--- dist/dais.components.js
+++ dist/dais.components.js
@@ -1,16 +1,4 @@
 /* global React */
-var CollapsibleContainer = React.createClass({
-  render: function() {
-    return (
-      <collapsible-container>
-        <div class="title-wrapper" onclick="this.classList.toggle('open')">Test Title</div>
-        <div class="content-wrapper" onclick="debugger">Test Content</div>
-      </collapsible-container>
-    );
-  }
-});
-
-/* global React */
 var InputContainer = React.createClass({
   render: function() {
     return (
@@ -26,6 +14,18 @@ var InputContainer = React.createClass({
         </error-container>
 
       </input-container>
+    );
+  }
+});
+
+/* global React */
+var CollapsibleContainer = React.createClass({
+  render: function() {
+    return (
+      <collapsible-container>
+        <div class="title-wrapper" onclick="this.classList.toggle('open')">Test Title</div>
+        <div class="content-wrapper" onclick="debugger">Test Content</div>
+      </collapsible-container>
     );
   }
 });
