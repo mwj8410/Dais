dcae738bd9b691f959d0b0a9274741e5edff3f80
mwj8410 <mwj8410@gmail.com>
2017-06-04 15:37:26 -0500

    fix(lint): correct jsx styling issues

--- ui/components/login.jsx
+++ ui/components/login.jsx
@@ -1,4 +1,4 @@
-import 'Style/components/login.scss';
+import 'Style/components/login.scss'
 
 import React, { Component } from 'react/dist/react'
 
@@ -10,53 +10,52 @@ class Login extends Component {
     }
 
     this.handleLogin = this.handleLogin.bind(this)
-  };
+  }
 
-  componentDidMount () {
+  componentDidMount() {
     window.requestAnimationFrame(() => {
       this.setState({
         secondaryClass: 'loaded'
-      });
-    });
-  };
+      })
+    })
+  }
 
-  handleLogin (event) {
+  handleLogin(event) {
     // TODO: configure redux ann get a real http request handler
-    let httpRequest = new XMLHttpRequest();
-    let form = document.forms[0];
-    event.preventDefault();
-    console.log(event);
+    let httpRequest = new XMLHttpRequest()
+    let form = document.forms[0]
+    event.preventDefault()
 
     // Uses absolute path because static resources are loaded from a sub dir
-    httpRequest.open('POST', `http://localhost:24601/user/login?user=${form.user.value}&password=${form.password.value}`, true);
-    httpRequest.send();
+    httpRequest.open('POST', `http://localhost:24601/user/login?user=${form.user.value}&password=${form.password.value}`, true)
+    httpRequest.send()
     httpRequest.onreadystatechange = () => {
       if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
           this.setState({
             secondaryClass: 'logged-in'
-          });
+          })
         } else {
           this.setState({
             secondaryClass: 'loaded error'
-          });
+          })
         }
       }
-    };
-  };
+    }
+  }
 
   render() {
     return (
       <div className={`login ${this.state.secondaryClass}`}>
         <form onSubmit={this.handleLogin}>
-          <input type="text" name="user" autoComplete="off" placeholder="User Name" />
-          <input type="password" name="password" placeholder="Password" />
-          <button type="submit">Login</button>
-          <div className="error">The credentials provided are invalid.</div>
+          <input type='text' name='user' autoComplete='off' placeholder='User Name' />
+          <input type='password' name='password' placeholder='Password' />
+          <button type='submit'>Login</button>
+          <div className='error'>The credentials provided are invalid.</div>
         </form>
       </div>
-    );
+    )
   }
 }
 
-export default Login;
+export default Login
